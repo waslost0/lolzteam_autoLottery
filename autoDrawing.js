556d253bfd247a7935decdb5bf8c2cf4fd5afe82
Mark <marcus.koh16.01.97@gmail.com>
2020-02-27 15:15:51 +0300

    added 2 scripts autoDrawing.js scroll_opentab.ts

---
+++ autoDrawing.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name         autoDrawing
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  a
+// @author       @waslost
+// @match        https://lolzteam.online/threads/*
+// @grant        window.close
+// ==/UserScript==
+
+function waitForElementToDisplay(time, alreadyText, thisHeadText) {
+
+    if (document.querySelector('.button.marginBlock.LztContest--Participate.primary')){
+        var imteToCLick = document.querySelector('.button.marginBlock.LztContest--Participate.primary')
+    }
+
+    if (document.querySelector('.Tooltip.PopupTooltip.LikeLink.item.control.like')) {
+        var LikeToCLick = document.querySelector('.Tooltip.PopupTooltip.LikeLink.item.control.like')
+    }
+
+    if (document.querySelector('.error.mn-15-0-0')) {
+        window.close();
+    }
+
+
+    var headings = document.evaluate("//span[contains(., 'Принять участие в розыгрыше')]", document, null, XPathResult.ANY_TYPE, null );
+    var thisHeading = headings.iterateNext();
+
+    if (alreadyText==null && thisHeadText==null && thisHeading==null) {
+        return;
+    }
+    if(alreadyText == null && thisHeadText != null) {
+            window.close();
+            return;
+    }
+
+    if(thisHeading != null) {
+        imteToCLick.click();
+        setTimeout(function() {
+           LikeToCLick.click();
+        }, 500);
+
+
+        setTimeout(function() {
+            window.close();
+        }, 1000);
+
+    }
+    else {
+        setTimeout(function() {
+            waitForElementToDisplay(time);
+        }, time);
+    }
+}
+
+(function() {
+    var alreadyIn = document.evaluate( "//span[@class='LztContest--alreadyParticipating button marginBlock alreadyParticipate disabled hidden']", document, null, XPathResult.ANY_TYPE, null );
+    var alreadyText = alreadyIn.iterateNext();
+
+    var headIn = document.evaluate("//span[contains(., 'Вы участвуете в конкурсе')]", document, null, XPathResult.ANY_TYPE, null );
+    var thisHeadText = headIn.iterateNext();
+    waitForElementToDisplay(1000, alreadyText, thisHeadText);
+})();
---
+++ scroll_opentab.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         scroll
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Just to scroll page down.
+// @author       @waslost
+// @match        https://lolzteam.online/forums/contests/
+// @grant        GM_openInTab
+// ==/UserScript==
+
+function scrollpage() {
+    var Height=document.documentElement.scrollHeight;
+    var i=1;
+    function f(){
+        window.scrollTo(0,i);
+        Height=document.documentElement.scrollHeight;
+        i=i+200;
+        if(i>=Height)
+        {
+            console.log('OPENNEWPAGE')
+            openNewPage();
+            return;
+        }
+        setTimeout( f,500);
+    }f();
+}
+
+function openNewPage() {
+    var dates = document.querySelectorAll('[id^="thread"]');
+    for (let i = 0; i < dates.length; i++) {
+        var alreadyIn = dates[i].innerHTML;
+        if (alreadyIn.search('alreadyParticipate') != -1) {
+            console.log("alreadyParticipate" + i);
+            continue;
+        } else {
+            setTimeout(function(i) {
+                console.log("not Participate");
+                var num = dates[i].attributes.id.nodeValue;
+                var splits = num.split('-');
+                GM_openInTab ('https://lolzteam.online/threads/' + splits[1]);
+                console.log('https://lolzteam.online/threads/' + splits[1]);
+
+            }, 500 * i, i);
+        }
+    }
+}
+
+scrollpage();
